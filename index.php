7299ba9ee8b3d519ffee428a4f4897178f1df81f
saikeerthireddy <37060511+saikeerthireddy@users.noreply.github.com>
2018-03-10 11:35:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html>
+	<head>
+		<title></title>
+		<link href="web.css" rel="stylesheet" type="text/css"/>
+		<link href="font-awesome icons/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+	<div class="main">
+	<!--start nav bar-->
+	
+		<div class= "navigation">
+			<div class="logo">MIRO<span>N:</span></div>
+			<div class="navbar">
+				<ul>
+					<li class="home"><a href="#">Home<i class="fa fa-angle-down "></i></a></li>
+					<li><a href="#">Portfolio<i class="fa fa-angle-down ">
+							<ul class="drag">
+								<li>services</li>
+								<li>services2</li>
+								<li>services3</li>
+								<li>services4</li>
+							</ul>
+					
+					
+					</i></a></li>
+					<li><a href="#">Blog<i class="fa fa-angle-down "></i></a></li>
+					<li><a href="#">Features<i class="fa fa-angle-down "></i></a></li>
+					<li><a href="#">pages<i class="fa fa-angle-down "></i></a></li>
+				</ul>
+			</div>
+			
+		</div><!--end of nav bar-->
+					<!--start slider-->
+		<div class="slider">
+				<img src="img_mountains_wide.jpg" alt="mountain"/>
+				<div class="image_content">Hello! Welcome to Miron</div>
+				<p class="p1">Miron is the theme you are looking for:multipurpose Layout</p>
+				<p class="p2"><a href="#">&nbsp Learn out&nbsp</a></p>
+				
+		</div>
+					<!--end of slider-->
+		<div class="content">
+				<div class="title">Miron Html5 Css3 Fully responsive Website template<div> with clean and valid code</div></div>
+				<div class="love"><i class="fa fa-heart-o" style="font-size:30px;padding-left:100px;color:red;"></i><br>
+										<span style="font-size:30px;color:black;line-height:50px">Make With Love</span><br>
+									Pellentesque in tempor metus.<br>
+									Vestibulum sit amet elit justo, vitae<br>
+									gravida erat. Nam hendrerit leo non enim<br>
+									consectetur tristique eleifend nec turpis.<br> 
+									Nulla fermentum, ante dapibus<br>
+									consectetur tristique eleifend nec turpis.<br>
+									consectetur tristique eleifend nec turpis.<br>
+									
+									</div>
+									
+				<div class="boot"><i class="fa fa-twitter-square" style="font-size:40px;padding-left:100px;color:red;"></i><br>
+										      <span style="font-size:30px;color:black;padding-left:40px;line-height:50px">Bootstrap3</span><br>
+
+									Pellentesque in tempor metusVestibulum sit <br>
+									amet elit justo, vitae gravida erat Nam hendrerit<br>
+									leo non enim consectetur tristique eleifend nec turpis<br>
+									Nulla fermentum, ante dapibus</div>
+									
+				<div class="clean"><i class="fa fa-code" style="font-size:40px;padding-left:100px;color:red"></i><br>
+										     <span style="font-size:30px;color:black;padding-left:40px"> Clean code</span><br>
+
+									Pellentesque in tempor metuss Vestibulum sit<br>
+									amet elit justo, vitae gravida erat. Nam hendrerit<br>
+									leo non enim consectetur tristique eleifend nec turpis<br>
+									Nulla fermentum, ante dapibus dyfgysddc <br>
+									consectetur tristique eleifend nec turpis.<br>
+								
+									</div>
+									
+				<div class="responsive"><i class="fa fa-television" style="font-size:40px;padding-left:100px;color:red"></i><br>
+										     <span style="font-size:30px;color:black;padding-left:40px"> Fully responsive </span><br>
+
+									Pellentesque in tempor metus. Vestibulum sit<br>
+									amet elit justo, vitae gravida erat Nam hendrerit<br>
+									leo non enim consectetur tristique eleifend nec turpis<br>
+									Nulla fermentum, ante dapibus ahghsgf ajdgajd adjagdja <br>
+				
+				</div>
+		
+		</div><!--end of content-->
+						<!--start images-->
+		<div class="images">
+				<div class="recent_work">Recently Work<br><i class="fa fa-file-image-o" style="color:red"></i></div><br>
+				<div class="describe">
+						<ul>
+							<li><span>&nbsp all</span></li>
+							<li>&nbsp Design</li>
+							<li> &nbspHtml5</li>
+							<li>&nbsp wordpress</li>
+							<li>&nbsp seo</li>
+						</ul>
+				</div>
+				<div class="gallery">
+					<div class="img123">
+						<img src="2.jpg" alt="pic1" width="30%" height="250px"/>&nbsp&nbsp&nbsp&nbsp&nbsp
+						<img src="download.jpg" alt="" width="30%" height="250px"/>&nbsp&nbsp&nbsp&nbsp&nbsp
+						<img src="images (6).jpg" alt="" width="30%" height="250px"/>
+					</div>
+					<div class="img456">
+						<img src="solo_female_traveller_26112015.jpg" alt="" width="30%" height="250px"/>&nbsp&nbsp&nbsp&nbsp&nbsp
+						<img src="73f476b9b3265962d725208b9a906846--preppy-clothes-casual-clothes.jpg" alt="" width="30%" height="250px"/>&nbsp&nbsp&nbsp&nbsp&nbsp
+						<img src="760303575 (1).jpg" alt="" width="30%" height="250px"/>
+					</div>
+				</div><!--end of gallery-->
+				<a href="#"><div class="portfolio"> View portfolio</div></a>
+				
+				
+			
+		
+		</div>
+						<!---end of image-->
+						<!-- start ABOUT US info-->
+		<div class="about_us">
+				<div class="title1">About Us<br><i class="fa fa-users"></i></div><br>
+				<div class="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pulvinar scelerisque dictum. Donec iaculis,<br>
+				diam sit amet suscipit feugiat, diam magna volutpat augue, sit amet varius nulla turpis sit amet lacus. Aenean faucibus, sem quis <br>
+				convallis lobortis. Aenean faucibus, sem quis convallis lobortis.</div><br>
+				<div class="icons">
+						<i class="fa fa-facebook fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-twitter fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-google-plus fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-users fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-wifi fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-linkedin fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-pinterest fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-skype fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-apple fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-yahoo fa-pull-left fa-border" style="font-size:20px"></i>
+						<i class="fa fa-vimeo fa-pull-left fa-border" style="font-size:20px"></i>
+						
+				</div><!--end of icons-->
+		</div><!--end of about us info-->
+		<div class="news">
+				<div class="latest_news">Latest News<br><i class="fa fa-pencil-square-o" style="color:red"></i></div>
+				<div class="gallery1">
+						<img src="2.jpg" alt="pic1" width="30%" height="250px"/>&nbsp&nbsp&nbsp&nbsp&nbsp
+						<img src="download.jpg" alt="" width="30%" height="250px"/>&nbsp&nbsp&nbsp&nbsp&nbsp
+						<img src="solo_female_traveller_26112015.jpg" alt="" width="30%" height="250px"/>
+				</div>
+				<div class="data">
+						<p><b>Amazing theme for Multi-Purpose</b><br>
+
+								aliqua.adipisicing elit, sed do eiusmod tempor <br>
+								incididunt ut labore et dolore magna.<br></p>
+							
+							<p><b>Amazing theme for Multi-Purpose</b><br>
+
+								aliqua.adipisicing elit, sed do eiusmod tempor <br>
+								incididunt ut labore et dolore magna.<br></p>
+							
+							<p><b>Amazing theme for Multi-Purpose</b><br>
+
+								aliqua.adipisicing elit, sed do eiusmod tempor <br>
+								incididunt ut labore et dolore magna.<br></p>
+				</div>
+				
+		</div>
+		<div class="footer">
+				<div class="about"><b>About us</b><br>
+
+									<p>Lorem ipsum ex vix illud nonummy,
+									novum tation et his. At vix scriptaset
+									patrioque scribentur, at pro fugit erts
+									verterem molestiae, sed et vivendo ali Lorem
+									ipsum ex vix illud nonummy, novum tation et his.
+									At vix scripta patrioque scribentur.</p></div>
+				<div class="latest_post"><b>Latest post</b><br><br>
+											
+										Blog slide post
+										<hr>
+										Lorem ipsum dollor
+										<hr>
+										Blog image post
+										<hr>
+										Blog Video post
+										<hr>
+										Lorem ipsum dollor</div>
+				<div class="latest_work">Latest Work<br><br>
+							<img src="img-1.jpg" alt="img1" width="55px"/>&nbsp&nbsp&nbsp
+							<img src="img-2.jpg" alt="img1" width="55px"/>&nbsp&nbsp&nbsp
+							<img src="img-3.jpg" alt="img1" width="55px"/><br>
+							<img src="img-4.jpg" alt="img1" width="55px"/>&nbsp&nbsp&nbsp
+							<img src="img-5.jpg" alt="img1" width="55px"/>&nbsp&nbsp&nbsp
+							<img src="img-6.jpg" alt="img1" width="55px"/><br>
+							<img src="img-1.jpg" alt="img1" width="55px"/>&nbsp&nbsp&nbsp
+							<img src="img-2.jpg" alt="img1" width="55px"/>&nbsp&nbsp&nbsp
+							<img src="img-3.jpg" alt="img1" width="55px"/>&nbsp&nbsp&nbsp
+				</div>
+				<div class="getintouch"><b>Get in touch</b><br><br>
+
+										<i class="fa fa-home" style="color:red"></i> 124,munnawali<br>
+										&nbsp&nbsp &nbspJaipur,302012<br>
+									
+										<i class="fa fa-phone" style="color:red"></i>   Phone: +91 1234567890<br>
+										<i class="fa fa-envelope" style="color:red"></i>  Email: mail@domain.com<br>
+										<i class="fa fa-globe" style="color:red"></i>  Website: http://www.site.com<br>
+										
+										<em class="fa fa-facebook fa-pull-left fa-border icon1" style="background-color:white;font-size:20px"></em>&nbsp&nbsp
+										<em class="fa fa-twitter fa-pull-left fa-border" style="background-color:white;font-size:20px"></em>&nbsp&nbsp
+										<em class="fa fa-instagram fa-pull-left fa-border" style="background-color:white;font-size:20px"></em>&nbsp&nbsp
+										<em class="fa fa-pinterest fa-pull-left fa-border" style="background-color:white;font-size:20px"></em>&nbsp&nbsp
+										<em class="fa fa-google-plus fa-pull-left fa-border" style="background-color:white;font-size:20px"></em>&nbsp&nbsp
+				</div>
+				<div class="copy">Mir<span>on:</span>
+					<p> &copy; copyright 2014.All right reserved.</p>
+				</div>
+		
+		</div>
+	
+	</div>	
+	
+	</body>
+</html>
